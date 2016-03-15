53ef7f74267cd7bc3460c513b51ea4731127868a
Denis Bazhenov <bazhenov@farpost.com>
2016-03-15 23:18:29 +1000

    Sleep mode added

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
 
 #define bit_get(p, m) (((p) >> m) & 1)
 #define bit_set(p, m) ((p) |= (1 << m))
@@ -8,26 +12,39 @@
 #define bit_flip(p, m) ((p) ^= (1 << m))
 #define bit_write(c, p, m) (c ? bit_set(p, m) : bit_clear(p, m))
 
-void initADC() {
-  ADMUX =
-            (1 << ADLAR) |     // left shift result
-            (0 << REFS1) |     // Sets ref. voltage to VCC, bit 1
-            (0 << REFS0) |     // Sets ref. voltage to VCC, bit 0
-            (0 << MUX3)  |     // use ADC2 for input (PB4), MUX bit 3
-            (0 << MUX2)  |     // use ADC2 for input (PB4), MUX bit 2
-            (1 << MUX1)  |     // use ADC2 for input (PB4), MUX bit 1
-            (0 << MUX0);       // use ADC2 for input (PB4), MUX bit 0
+void setup() {
+  cli();
 
-  ADCSRA = 
-            (1 << ADEN)  |     // Enable ADC 
-            (0 << ADPS2) |     // set prescaler
-            (0 << ADPS1) |     // set prescaler
-            (0 << ADPS0);      // set prescaler
+  bit_clear(MCUSR, WDRF); // Clear the reset flag
+  
+  /* Enabling watchdog */ {
+    WDTCR |= _BV(WDCE) | _BV(WDE);
+    WDTCR = _BV(WDP3) | _BV(WDP0) | _BV(WDIE);
+
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   /* EDIT: could also use SLEEP_MODE_PWR_DOWN for lowest power consumption. */
+  }
+  
+  sei();
 }
 
 int readAdc() {
-	ADCSRA |= (1 << ADSC);         // start ADC measurement
-	while (ADCSRA & (1 << ADSC) ); // wait till conversion complete
+  ADMUX =
+    (1 << ADLAR) |     // left shift result
+    (0 << REFS1) |     // Sets ref. voltage to VCC, bit 1
+    (0 << REFS0) |     // Sets ref. voltage to VCC, bit 0
+    (0 << MUX3)  |     // use ADC2 for input (PB4), MUX bit 3
+    (0 << MUX2)  |     // use ADC2 for input (PB4), MUX bit 2
+    (1 << MUX1)  |     // use ADC2 for input (PB4), MUX bit 1
+    (0 << MUX0);       // use ADC2 for input (PB4), MUX bit 0
+
+  ADCSRA = 
+    (1 << ADEN)  |     // Enable ADC 
+    (0 << ADPS2) |     // set prescaler
+    (0 << ADPS1) |     // set prescaler
+    (0 << ADPS0);      // set prescaler
+
+  bit_set(ADCSRA, ADSC);          // start ADC measurement
+	while (ADCSRA & (1 << ADSC));  // wait till conversion complete
 	return ADCH;
 }
 
@@ -36,6 +53,24 @@ char isLatchClosed() {
   int value = abs(readAdc() - 128);
   bit_clear(PORTB, 2);            // power down hall sensor
   return value <= 5;
+}
+
+void enterSleep() {
+  // Disabling ADC
+  ADCSRA = 0;
+
+  // Disabling all peripherals
+  PORTB = 0;
+  
+  // Disable Brown out Detection
+  sleep_bod_disable();
+  
+  wdt_reset();
+  /* Now enter sleep mode. */
+  sleep_mode();
+  
+  /* Re-enable the peripherals. */
+  power_all_enable();
 }
 
 void beep() {
@@ -55,16 +90,18 @@ void ledIndicate() {
 }
 
 int	main(void) {
-	initADC();
+	setup();
 	DDRB = _BV(PINB2) | _BV(PINB3) | _BV(PINB1);
   PORTB = 0;            // disable all pull ups and clearing ouput
 
   for (;;) {
+    cli();
+    ledIndicate();
 		if (isLatchClosed()) {
 			beep();
 		}
-    ledIndicate();
-    _delay_ms(1000);
+    sei();
+    enterSleep();
 	}
 	return 0;
 }
